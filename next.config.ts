544beeda064bb909523acfe2a38ccc5fb09d0216
Md Asharaf <718romanempire@gmail.com>
2025-10-28 03:17:46 +0530

    feat: add components.json for configuration and styling
    feat: update next.config.ts to allow images from Cloudinary
    
    chore: add new dependencies including Radix UI components and TanStack Query
    
    style: enhance globals.css with new color variables and animations
    
    refactor: remove default page.tsx and create auth and query providers
    
    feat: implement AuthProvider for user authentication context
    
    feat: add QueryProvider for managing server state with React Query
    
    types: add global CSS module declaration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
